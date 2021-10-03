1d82e1bdb050cdde1b2bbcd45dee5dd1c359cfa7
Wildy Estephan <westephan50@gmail.com>
2021-10-02 21:33:31 -0400

    17

--- soc_014/models/models.py
+++ soc_014/models/models.py
@@ -48,7 +48,7 @@ class Tema(models.Model):
         else:
             alumnos.append([4, self.env.user.id])
 
-        self.env.sudo().user.selecciono = True
+        self.sudo().env.user.selecciono = True
 
         self.alumno_ids = alumnos
 
